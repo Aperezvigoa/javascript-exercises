92bf2c270bdb3db25c875c598d95ee89f7b3d042
Aperezvigoa <108186387+Aperezvigoa@users.noreply.github.com>
2025-08-04 18:13:35 +0200

    Solved using loop and conditionals

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeated = "";
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            repeated = repeated.concat(string);
+    }
+        return repeated;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
