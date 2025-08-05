850cba9521c9f8cd4a5c9a9731fb5f28dedd3d86
Aperezvigoa <108186387+Aperezvigoa@users.noreply.github.com>
2025-08-05 14:40:21 +0200

    Added solution to the reverse string exercise using split, reverse and join

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splittedArr = str.split("");
+    return splittedArr.reverse().join("");
 };
+
+console.log(reverseString("hola soy Alex"));
 
 // Do not edit below this line
 module.exports = reverseString;
