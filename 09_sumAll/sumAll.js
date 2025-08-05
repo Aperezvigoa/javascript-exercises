a2bb7d3a52ea216ae359c8917bb29dddb277c4e6
Aperezvigoa <108186387+Aperezvigoa@users.noreply.github.com>
2025-08-05 16:06:42 +0200

    Added solution using conditionals and loops

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+    return "ERROR";
+  } else {
+    let max = a > b ? a : b;
+    let min = a < b ? a : b;
+    let number = 0;
 
+    for (min; min <= max; min++) {
+      number += min;
+    }
+    return number;
+  }
 };
 
 // Do not edit below this line
