3fbd015f18246af340b0d9210bbf5d38a7683fd2
Aperezvigoa <108186387+Aperezvigoa@users.noreply.github.com>
2025-08-05 15:22:51 +0200

    Added solution using loops, slice, join and includes

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+
+    let arr = array;
+    for (const arg of theArgs) {
+        while (arr.includes(arg)) {
+            let index = arr.indexOf(arg);
+            let leftSide = arr.slice(0, index);
+            let rightSide = arr.slice(index + 1);
+            arr = leftSide.concat(rightSide);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
