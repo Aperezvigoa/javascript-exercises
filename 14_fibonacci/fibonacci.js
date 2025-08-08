15818626d97e9ad78c9cf07f086d87449b142401
Aperezvigoa <108186387+Aperezvigoa@users.noreply.github.com>
2025-08-08 13:21:43 +0200

    Fibonacci solved

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    
+    if (n === 0 || n === "0") {
+        return 0;
+    } else if ( n < 0)  {
+        return "OOPS";
+    } else {
+        let index = typeof(n) === "string" ? parseInt(n) : n;
+        let fib = [1, 1];
+        for (let i = 0; i < index; i++) {
+            let temp = fib[i] + fib[i+1];
+            fib.push(temp);
+        }
+        return fib[index-1];
+    }
+    
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+        
